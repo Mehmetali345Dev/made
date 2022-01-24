6f07176f1bc46ea1ce9c34dfcfe19402becc64ab
Mehmet Ali Külahçı <345m.ali2007@gmail.com>
2022-01-24 22:01:22 +0300

    feat: add faq

---
+++ pages/faq.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex flex-col w-full">
+    <h1 class="text-2xl font-bold">Frequently Asked Questions</h1>
+    <div class="grid md:grid-cols-2 gap-3 mt-2">
+      <QuestionCard
+        v-for="(question, index) in questions"
+        :key="index"
+        :question="question"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FaqPage',
+  data() {
+    return {
+      questions: [
+        {
+          q: 'What is MADE?',
+          a: 'MADE is a showcase for your dotfiles. You can find it on Github, and you can add your dotfiles to it.',
+        },
+        {
+          q: 'How do I add my dotfiles?',
+          a: 'You can add your dotfiles to MADE by fork this repository and add your .md file with your Github name, after that open pull request to me.',
+        },
+        {
+          q: 'Is MADE open source?',
+          a: 'Yes, you can find it on Github.',
+        },
+        {
+          q: 'How do I contribute?',
+          a: 'You can find contribute guidlines on Github.',
+        },
+        {
+          q: 'Who is behind MADE?',
+          a: 'Mehmet, who is also the creator of this project.',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
