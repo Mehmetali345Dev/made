d93dd97bf411a4d4b6313ee8fba17a67d391d071
Mehmet Ali Külahçı <345m.ali2007@gmail.com>
2022-01-29 22:31:19 +0300

    fix: responsive issues

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,16 +3,16 @@
     class="flex transition-all flex-col items-center dark:bg-dark-900 text-black dark:text-white bg-gray-100 min-h-screen"
   >
     <Navbar />
-    <WikiExplorer v-if="$route.params.pathMatch" class="md:hidden" />
+    <WikiExplorer v-if="$route.params.pathMatch" class="w-11/12 mb-2 md:hidden" />
     <div
       class="flex-grow flex justify-center items-start w-11/12 gap-3 md:py-8 md:px-16 p-4"
     >
-      <div class="w-11/12">
+      <div class="flex gap-3 w-11/12">
         <WikiExplorer
           v-if="$route.params.pathMatch"
           class="hidden md:block min-w-48"
         />
-        <Nuxt />
+        <Nuxt class="w-full" />
       </div>
     </div>
   </div>
