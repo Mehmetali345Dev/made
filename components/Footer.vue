3d70ca7bf8dd07dafa7c583c664b04f6beb1f10f
Mehmet Ali Külahçı <345m.ali2007@gmail.com>
2022-02-06 14:50:47 +0300

    refactor: links

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,14 +2,18 @@
   <div class="md:py-8 md:px-16 p-4 w-full">
     <div class="md:flex gap-3 justify-between items-center w-full">
       <a
-        to="https://345dev.me"
+        href="https://345dev.me"
+        target="_blank"
+        rel="noopener noreferrer"
         class="text-lg font-bold hover:text-blue-500 transition-all ease-in-out duration-300"
       >
         Made by Mehmetali345Dev
       </a>
       <div class="font-bold text-lg flex gap-3">
         <a
-          to="https://github.com/mehmetali345dev/made"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/mehmetali345dev/made"
           class="text-lg font-bold hover:text-blue-500 transition-all ease-in-out duration-300"
         >
           View on Github
